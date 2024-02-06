9a65411b7469d5129dc52878b2ddfdae429840c9
jimkao <Jim87as0821@gmail.com>
2024-02-06 14:00:50 +0800

    修改path

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import { createApp } from "https://unpkg.com/vue@3/dist/vue.esm-browser.js";
 
 const url = "https://vue3-course-api.hexschool.io/v2";
-const path = "lovecorgi";
+const path = "";
 
 import pagination from "./components/pagination.js";
 import productModal from "./components/productModal.js";
